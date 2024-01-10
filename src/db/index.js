e4d141a56ef1a67301548ef862fcb8b024c75908
Abhinav <thenextabhinav>
2024-01-10 19:46:46 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { DB_NAME } from "../constants.js";
+dotenv.config(); // Load environment variables from .env file
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+
+    } catch (error) {
+        console.log("MONGODB connection FAILED", error);
+        process.exit(1);
+    }
+}
+
+
+export default connectDB;
